1ea32324f32714e4a8c9f2bad1ac7ccd5831bce4
Anh Duy <hduy9863@gmail.com>
2026-06-06 13:28:07 +0700

    Update

--- src/main/java/com/example/chatWeb/controller/HealthCheckController.java
+++ src/main/java/com/example/chatWeb/controller/HealthCheckController.java
@@ -3,16 +3,20 @@ package com.example.chatWeb.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
 public class HealthCheckController {
 
-    @GetMapping("/health")
-    public ResponseEntity<Map<String, String>> healthCheck() {
-        return ResponseEntity.ok(Map.of("status", "UP", "message", "Server is running smoothly!"));
+    @RequestMapping(value = "/health", method = {RequestMethod.GET, RequestMethod.HEAD})
+    public ResponseEntity<Map<String, String>> checkHealth() {
+        Map<String, String> status = new HashMap<>();
+        status.put("status", "UP");
+        return ResponseEntity.ok(status);
     }
 }
